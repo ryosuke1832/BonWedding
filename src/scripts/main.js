dacdc82cb48be398eecea8ca9941ff002931d00e
ryosuke1832 <84903480+ryosuke1832@users.noreply.github.com>
2023-06-11 10:38:19 +0900

    0611 トップページ修正

---
+++ src/scripts/main.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburgerMenu = document.getElementById('hamburger-menu');
+    const sidebar = document.getElementById('sidebar');
+    const overlay = document.getElementById('overlay');
+
+    hamburgerMenu.addEventListener('click', function() {
+        sidebar.classList.add('open');
+        overlay.style.display = 'block';
+        document.body.style.overflow = 'hidden';
+    });
+
+    overlay.addEventListener('click', function() {
+        sidebar.classList.remove('open');
+        this.style.display = 'none';
+        document.body.style.overflow = 'auto';
+    });
+});
